99441222c5fb5c730bc9bebedcabbb78b70b98b4
Sfixy <chaoranff@163.com>
2020-10-22 21:37:35 +0800

    shy auto save

--- PAT/L1-010 比较大小.cpp
+++ PAT/L1-010 比较大小.cpp
@@ -1,7 +1,53 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
+//#include<iostream>
+//#include<cstdio>
+//#include<cstring>
+//#include<vector> 
+//
+//using namespace std;
+//øÏ≈≈ 
+//int main()
+//{
+//	
+//    vector<long> v;
+//	
+//	for(int i = 0;i < 3;i++)
+//	{
+//		long a;
+//		cin >> a;
+//		v.push_back(a);
+//	}
+//	
+//	long left = 0;
+//	long right = 2;
+//	long temp = v[left];
+//	long i = left;
+//	long j = right;
+//	while(i != j)
+//	{
+//		while(v[j] >= temp && i < j) j--;
+//		while(v[i] <= temp && i < j) i++;
+//		
+//		if(i < j)
+//		{
+//			temp = v[i];
+//			
+//			v[i] = v[j];
+//			v[j] = temp;
+//		} 
+//	}
+//	v[left] = v[i];
+//	v[i] = temp;
+//	
+//	for(int x = 0; x < 2; x++)
+//	{
+//		cout << v[x] << "->";
+//	}
+//	cout << v[2];
+//	
+//    return 0;
+//}
 
+#include<iostream>
 using namespace std;
 
 void swap(int &x,int &y)
@@ -13,13 +59,12 @@ void swap(int &x,int &y)
 
 int main()
 {
-	
-    int a,b,c;
+	int a,b,c;
 	cin >> a >> b >> c;
-	if(a > b)
-	{
-		swap(a,b);
-	}
+	if (c < b) swap(c,b);
+	if (c < a) swap(c,a);
+	if (a > b) swap(a,b);
 	cout << a << "->" << b << "->" << c;
-    return 0;
+	
+	return 0;
 }
